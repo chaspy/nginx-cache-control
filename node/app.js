29d6a9996b246c82bfdf5b7315fd79e7bfdda086
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-18 21:05:29 +0900

    Set max-age=60

--- node/app.js
+++ node/app.js
@@ -5,6 +5,11 @@ var server = app.listen(3000, function(){
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+app.use(function (req, res, next) {
+  res.setHeader("Cache-Control", ["private", "max-age=60"].join(','));
+	next();
+});
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res, next){
